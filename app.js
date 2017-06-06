4d45c06056844dbe1e225e4ce42c1f933e2f52f6
saxon17 <121659994@qq.com>
2017-06-06 11:31:27 +0800

    dev0.1
    增加用户登录

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
 //app.js
+var wilddog = require('./wilddog-weapp-all')
+
 App({
+
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
+  var that = this
+  //野狗配置
+  var config = {
+      syncURL: 'https://xm7.wilddogio.com',
+      authDomain: 'xm7.wilddog.com'
+    }
+
+
+    wilddog.initializeApp(config)
+    wilddog.auth().signInWeapp().then(function(user){
+    console.log("我要打印user");
+      console.log(that)
+      // console.log(user);
+      that.my_userInfo = user;
+
+
+      // console.log("怎么打印不出来");
+
+      // console.log(that)
+
+
+    }).catch(function(err){
+   
+    })
+    this.ref = wilddog.sync().ref("/xiamiao/activity")
+
+
+
+
+
+  //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
+
+  //将野狗数据库对象绑定到app.ref上
+    getDataList: function() {
+    return this.ref
+  },
+
+ 
+ //从全局变量中获取用户登录信息
+  getMyUser:function(){
+    return this.my_userInfo
+  },
+
+
   getUserInfo:function(cb){
     var that = this
     if(this.globalData.userInfo){
@@ -16,7 +62,11 @@ App({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
+
               that.globalData.userInfo = res.userInfo
+
+              //  console.log('getUserInfo',that.globalData.userInfo)
+
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
           })
@@ -27,4 +77,8 @@ App({
   globalData:{
     userInfo:null
   }
+
+
+
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,20 @@
 //index.js
+// var wilddog = require('../../wilddog-weapp-all')
+// var config = {
+//   syncURL: 'https://xm7.wilddogio.com',
+//   authDomain: 'xm7.wilddog.com'
+// }
+// wilddog.initializeApp(config);
+// var ref = wilddog.sync().ref("/xiamiao/activity");
+
+
 //获取应用实例
 var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    activityList:[],
+    act_type: "类型",
     userInfo: {}
   },
   //事件处理函数
@@ -14,13 +25,48 @@ Page({
   },
   onLoad: function () {
     console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    
+
+
+
+    //   var that = this
+    // //调用应用实例的方法获取全局数据
+    // app.getUserInfo(function(userInfo){
+    //   //更新数据
+    //   that.setData({
+    //     userInfo:userInfo
+    //   })
+    // })
+  
+      
+    // })
+
+    console.log("我的用户：")
+
+   
+    console.log(app.getMyUser())
+  
+    // var name = app.globalData.userInfo.nickName
+    // console.log("GlobalData:");
+    // var userInfo = app.globalData.userInfo;
+  //  console.log(this.userInfo);
+
+    var ref = app.getDataList()
+    console.log("获取数据");
+    // console.log(ref);
+
+    ref.on('value', function (snapshot) {
+      var val = snapshot.val();
+      if (val !== null) {
+        this.setData({
+          activityList: val
+        })
+      } else {
+        console.log("未获得数据");
+      }
+      console.log(this.data.activityList); //后执行，有数据
+    }, this)
+
+  },
 })
+
